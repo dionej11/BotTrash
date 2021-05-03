3cbcd39a204f0ca29b4db2df3215795e8c95e3e5
Isabela Ceballos <isaceballos2001@gmail.com>
2021-05-03 14:22:53 -0500

    ingreso nuevas pantallas

---
+++ app/src/main/java/com/example/bottrash/cuarta_activity.java
@@ -0,0 +1,14 @@
+package com.example.bottrash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cuarta_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuarta_activity);
+    }
+}
---
+++ app/src/main/java/com/example/bottrash/quinta_activity.java
@@ -0,0 +1,14 @@
+package com.example.bottrash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class quinta_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quinta_activity);
+    }
+}
